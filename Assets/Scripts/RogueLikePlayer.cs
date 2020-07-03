e43d0ec2e056c08b0064e28ee81f0e6af832831a
Lostage3 <lostage30030@gmail.com>
2020-07-03 11:05:46 +0200

    RogueLike

---
+++ Assets/Scripts/RogueLikePlayer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RogueLikePlayer : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 10;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Movement();
+    }
+
+    private void Movement()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        Vector3 moveVector = new Vector3(h, 0, v);
+
+        transform.position += moveVector * moveSpeed * Time.deltaTime;
+    }
+}
